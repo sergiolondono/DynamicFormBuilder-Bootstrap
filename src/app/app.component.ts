4efd869d9eb8bb34b3c80960cfb176c043c930e4
Londono Gallo Sergio Andres <Sergio.Londono@carvajal.com>
2019-06-12 17:49:41 -0500

    Expresion regular para formato de fecha dd/MM/yyyy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,9 @@ export class AppComponent implements OnInit {
       name: 'date',
       label: 'Date',
       value: '',
-      required: true
+      required: true,
+      hasPattern: true,
+      pattern: '^(((((0[1-9])|(1\d)|(2[0-8]))\/((0[1-9])|(1[0-2])))|((31\/((0[13578])|(1[02])))|((29|30)\/((0[1,3-9])|(1[0-2])))))\/((20[0-9][0-9])|(19[0-9][0-9])))|((29\/02\/(19|20)(([02468][048])|([13579][26]))))$'
      },
      {
       type: 'datetext',
@@ -286,7 +288,7 @@ export class AppComponent implements OnInit {
     }
 ];
    
-public fields: any = this.dynamicsFields;
+public fields: any = this.dynamicsFields2;
 
   constructor(private rest: DocumentFieldsService) {
     
